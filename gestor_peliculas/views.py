88a65a2f348acd6d5a57330f3ae911efccfaace6
Raul <raul.r.a.fp@gmail.com>
2017-10-25 20:15:20 +0200

    Intento de hacer enviar Json

--- gestor_peliculas/views.py
+++ gestor_peliculas/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+import json
 
 from .models import Pelicula, Actor
 
 def pelis(request):
-    lista_pelis = Pelicula.objects.all()
-    datos = serializers.serialize("json", lista_pelis)
-    return render(request, 'gestorPeliculas/peliculas.html', {'datos': datos})
+    datos = serializers.serialize("json", Pelicula.objects.all())
+    json_datos = json.dumps(datos)
+    return JsonResponse(datos, safe=False)
