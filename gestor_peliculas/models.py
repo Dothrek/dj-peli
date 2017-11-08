de4bd891a8baae5000630944640a60bd1e56bfb9
Raul <raul.r.a.fp@gmail.com>
2017-11-08 21:59:18 +0100

    muestro peliculas con cada uno de sus actores

--- gestor_peliculas/models.py
+++ gestor_peliculas/models.py
@@ -1,17 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class Pelicula(models.Model):
-    titulo = models.CharField(max_length=50)
-    imagen = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=200)
-    actores = models.ManyToManyField('Actor')
-    def __str__(self):
-        return self.titulo
 
 class Actor(models.Model):
     nombre = models.CharField(max_length=50,)
     fecha_nac = models.IntegerField()
     foto = models.CharField(max_length=100)
+
     def __str__(self):
         return self.nombre
+
+    def to_api_reference(self):
+        return  {"id": self.pk, "name": self.nombre}
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=50)
+    imagen = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
+    actores = models.ManyToManyField(Actor)
+
+    def __str__(self):
+        return self.titulo
+
+    def to_api_obj(self):
+        return {
+            "titulo": self.titulo,
+            "imagen": self.imagen,
+            "descripcion": self.descripcion,
+            "actores": list(map(lambda x : x.to_api_reference(), self.actores.all()))
+        }
+
+    def to_api_reference(self):
+        return  {"id": self.pk, "tiulo": self.titulo}
--- gestor_peliculas/views.py
+++ gestor_peliculas/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.core import serializers
-import json
 
 from .models import Pelicula, Actor
 
 def pelis(request):
-    datos = serializers.serialize("json", Pelicula.objects.all())
-    #   array_datos = json.loads(datos)
-    # new_array_datos = []
-    # for elem in array_datos:
-    #     new_array_datos.append(elem["fields"])
+    peliculas = Pelicula.objects.prefetch_related('actores')
 
-    new_array_datos = list(map(lambda x : x["fields"], json.loads(datos)))
+    pelis = list(map(lambda x : x.to_api_obj(), peliculas))
 
-    return JsonResponse(new_array_datos, safe=False)
-
-def p2(request):
-    data = serializers.serialize("json", Pelicula.objects.all())
-    return HttpResponse(data, content_type='application/json')
+    return JsonResponse(pelis, safe=False)
